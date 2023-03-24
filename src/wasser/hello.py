c8120ec9efa763a6fd6f56aa3347005fc187d5ba
Alexander Juda <alexanderjuda@gmail.com>
2023-03-24 22:18:12 +0100

    Simulate a couple energies

--- src/wasser/hello.py
+++ src/wasser/hello.py
@@ -2,12 +2,18 @@ from pyscf import scf, gto
 
 
 def main():
-    mol_h2o = gto.M(atom="O 0 0 0; H 0 1 0; H 0 0 1", basis="ccpvdz")
-    rhf_h2o = scf.RHF(mol_h2o)
-    e_h2o = rhf_h2o.kernel()
+    for atom in [
+        "O 0 0 0; H -1 0 0; H 1 0 0",
+        "O 0 1 0; H -1 0 0; H 1 0 0",
+        "O 0 0.6 0; H -0.6 0 0; H 0.6 0 0",
+    ]:
+        mol_h2o = gto.M(atom=atom, basis="ccpvdz")
+        rhf_h2o = scf.RHF(mol_h2o)
+        e_h2o = rhf_h2o.kernel()
 
-    print(f"{mol_h2o.atom =}")
-    print(f"energy = {e_h2o}")
+        print(f"{mol_h2o.atom =}")
+        print(f"energy = {e_h2o:.3f}")
+        print()
 
 
 if __name__ == "__main__":
